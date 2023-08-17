e559c0156f6348207bb6883c9ce7151b6aaab42d
Emanuel moratti <morattiemanuel@gmail.com>
2023-08-17 19:51:42 -0300

    tercera entrega

--- js/main.js
+++ js/main.js
@@ -1,64 +1,59 @@
-// Monedas disponibles
-function mostrarMonedas(monedas) {
-    monedas.forEach((moneda) => {
-        console.log(`Nombre: ${moneda.nombre} - Valor: ${moneda.valor}${moneda.simbolo} - País: ${moneda.pais}`);
+document.addEventListener('DOMContentLoaded', () => {
+    const conversionRates = {
+        usd: 0.00286, //1 USD equivale a 350 ARS apróx.
+        eur: 0.0026, //1 EUR equivale a 380 ARS apróx.
+        brl: 0.014 //1 BRL equivale a 70 ARS apróx.
+    };
+
+    const convertButton = document.getElementById('convert');
+    const amountInput = document.getElementById('amount');
+    const currencySelect = document.getElementById('currency');
+    const resultDiv = document.getElementById('result');
+    const historyDiv = document.getElementById('history');
+
+    const conversionHistory = [];
+
+    convertButton.addEventListener('click', () => {
+        const amount = parseFloat(amountInput.value);
+        const selectedCurrency = currencySelect.value;
+
+        if (isNaN(amount)) {
+            resultDiv.innerHTML = 'Ingrese un monto válido.';
+            return;
+        }
+
+        const convertedAmount = amount * conversionRates[selectedCurrency];
+
+        const resultText = `${amount.toFixed(2)} ARS equivalen a ${convertedAmount.toFixed(2)} ${selectedCurrency.toUpperCase()}.`;
+        resultDiv.innerHTML = resultText;
+
+        const conversionData = {
+            amount: amount.toFixed(2),
+            currency: selectedCurrency.toUpperCase(),
+            result: resultText
+        };
+
+        conversionHistory.push(conversionData);
+        localStorage.setItem('conversionHistory', JSON.stringify(conversionHistory));
+
+        updateHistory();
     });
-}
 
-// Filtrar moneda por nombre
-function filtrarMoneda(nombreMoneda) {
-    const resultado = monedas.filter((moneda) => moneda.nombre.toLowerCase() === nombreMoneda.toLowerCase());
-    return resultado;
-}
+    function updateHistory() {
+        historyDiv.innerHTML = '';
 
-// Convertir pesos a la moneda seleccionada
-function convertirPesosAMoneda(pesos, valorTasaCambio) {
-    return pesos / valorTasaCambio;
-}
-
-// Función principal
-function convertir() {
-    let continuar = true;
-
-    while (continuar) {
-        // Obtener la moneda en la que se desea convertir los pesos
-        const nombreMoneda = prompt("Ingrese la moneda que quiere obtener (Dolar - Euro - Libra Esterlina - Yen Japones - Real):");
-
-        // Filtrar la moneda por nombre
-        const monedaFiltrada = filtrarMoneda(nombreMoneda);
-
-        // Verificar si la moneda existe en la lista de monedas
-        if (monedaFiltrada.length === 0) {
-            console.log("Moneda no encontrada");
-            continue;
-        }
-        // Para poder usar el simbolo en el console.log
-        simboloMoneda = monedaFiltrada[0].simbolo;
-
-        // Mostrar la información de la moneda obtenida del array
-        mostrarMonedas(monedaFiltrada);
-
-        // Obtener la cantidad de pesos argentinos a convertir
-        const pesosArgentinos = parseFloat(prompt("Ingrese la cantidad de pesos argentinos a convertir:"));
-
-        // Verificar si ingresó un monto válido
-        if (isNaN(pesosArgentinos) || pesosArgentinos <= 0) {
-            console.log("Por favor, ingrese una cantidad válida mayor a 0.");
-            continue;
-        }
-
-        // Obtener la tasa de cambio de la moneda seleccionada
-        const tasaCambioMoneda = monedaFiltrada[0].valor;
-
-        // Convertir pesos argentinos a la moneda seleccionada
-        const cantidadMoneda = convertirPesosAMoneda(pesosArgentinos, tasaCambioMoneda);
-        console.log(`${pesosArgentinos} pesos argentinos son equivalentes a ${simboloMoneda}${cantidadMoneda.toFixed(2)}.`);
-
-        // Preguntar al usuario si desea continuar
-        const opcion = prompt("¿Desea realizar otra conversión? (s/n)");
-        if (opcion.toLowerCase() !== "s") {
-            continuar = false;
-        }
+    for (const conversion of conversionHistory) {
+        const historyItem = document.createElement('div');
+        historyItem.classList.add('history-item');
+        historyItem.textContent = `${conversion.amount} ARS a ${conversion.currency}: ${conversion.result}`;
+        historyDiv.appendChild(historyItem);
     }
-}
-convertir();
+    }
+
+    // Cargar historial desde el almacenamiento local al cargar la página
+    const savedHistory = localStorage.getItem('conversionHistory');
+    if (savedHistory) {
+        conversionHistory.push(...JSON.parse(savedHistory));
+        updateHistory();
+    }
+});
